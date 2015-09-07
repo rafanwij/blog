47d8ecb4d6246341a5c24f0c088193dec6e2992f
rafanwij <rafanwij@gmail.com>
2015-09-07 14:41:33 +0700

    7sept

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,10 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class dashboard extends CI_Controller {
-	public function __construct(){
-		parent:: __construct();
-		$this->load->library(array('session','encrypt'));
-	}
 	public function index()
 	{	
 		$this->session->unset_userdata('sumPage','currentPage');
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,11 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class login extends CI_Controller {
-	public function __construct(){
-		parent:: __construct();
-		$this->load->library(array('session','encrypt'));
-	}
-
 	public function index()
 	{	
 		if($this->session->userdata('username') != null && $this->session->userdata('username') !="")
